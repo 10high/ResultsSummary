76223183ebae08e0602ef82cd9ba4c1c8b566248
Michael Waaler <michael@flyingtens.com>
2023-03-04 20:13:02 +0100

    general cleanup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Result } from "./components/result/result";
+import { SummaryPanel } from "./components/summary/summaryPanel/summaryPanel";
+
+
+ReactDOM.render(
+    <main>
+        <div className="imposter">
+            <div className="card">
+                <Result />
+                <SummaryPanel />
+            </div>
+        </div>
+    </main>,
+    document.getElementById("main"));
 
