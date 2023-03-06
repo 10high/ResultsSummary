baf8713f5685cabab1d95c327265b11c4df07c94
Michael Waaler <michael@flyingtens.com>
2023-03-06 19:57:18 +0100

    SVG icons now added dynamically

--- src/components/summary/summaryItem/summaryItem.js
+++ src/components/summary/summaryItem/summaryItem.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./summaryItem.css";
 
+
 export class SummaryItem extends React.Component {
     render() {
         return (
             <article className="summaryItem__outerWrapper">
                 <div className="summaryItem__innerWrapper">
-                    <span className="summaryItem__icon">{this.props.icon}</span>
-                    <h2 className="summaryItem__header">{this.props.category}</h2>
+                    <div className="summaryItem__categoryWrapper">
+                        <img className="summaryItem__icon" alt="" src={this.props.icon} />
+                        <h2 className="summaryItem__header">{this.props.category}</h2>
+                    </div>
                     <p className="summaryItem__scoreWrapper">
                         <span className="summaryItem__score">{this.props.score}</span>
                         <span className="summaryItem__scoreOutOf">/ 100</span>
