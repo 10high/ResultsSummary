a720d49427657e77a96d2b2831ff14a490f631d0
Michael Waaler <michael@flyingtens.com>
2023-03-06 19:53:57 +0100

    svg icons now added dynamically

--- src/components/summary/summaryPanel/summaryPanel.js
+++ src/components/summary/summaryPanel/summaryPanel.js
@@ -2,11 +2,29 @@ import React from "react";
 import "./summaryPanel.css"
 import data from "../../../data/data.json"
 import { SummaryItem } from "../summaryItem/summaryItem";
+import reactionIcon from "../../../assets/images/icon-reaction.svg";
+import memoryIcon from "../../../assets/images/icon-memory.svg";
+import verbalIcon from "../../../assets/images/icon-verbal.svg";
+import visualIcon from "../../../assets/images/icon-visual.svg";
+
+
 
 export class SummaryPanel extends React.Component {
     getData(category) {
         for (let item of data) {
-            if (item.category.toLowerCase() === category) return item;
+            if (item.category.toLowerCase() === category) {
+                return item;
+            }
+        }
+    }
+
+    getIcon(category) {
+        const svgVars = { reaction: reactionIcon, memory: memoryIcon, verbal: verbalIcon, visual: visualIcon };
+        for (let item of data) {
+            if (item.category.toLocaleLowerCase() === category) {
+                console.log(svgVars[category]);
+                return svgVars[category];
+            }
         }
     }
 
@@ -17,22 +35,22 @@ export class SummaryPanel extends React.Component {
                 <div className="summaryPanel__itemsWrapper">
 
                     <SummaryItem className="summaryPanel__itemReaction"
-                        icon={this.getData("reaction").icon}
+                        icon={this.getIcon("reaction")}
                         category={this.getData("reaction").category}
                         score={this.getData("reaction").score} />
 
                     <SummaryItem className="summaryPanel__itemMemory"
-                        icon={this.getData("memory").icon}
+                        icon={this.getIcon("memory")}
                         category={this.getData("memory").category}
                         score={this.getData("memory").score} />
 
                     <SummaryItem className="summaryPanel__itemVerbal"
-                        icon={this.getData("verbal").icon}
+                        icon={this.getIcon("verbal")}
                         category={this.getData("verbal").category}
                         score={this.getData("verbal").score} />
 
                     <SummaryItem className="summaryPanel__itemVisual"
-                        icon={this.getData("visual").icon}
+                        icon={this.getIcon("visual")}
                         category={this.getData("visual").category}
                         score={this.getData("visual").score} />
                 </div>
